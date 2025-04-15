6c9331dc7dc79e17b7a155cbabb47b854c3a0975
Что такое import [#505278] <larinand97@gmail.com>
2025-04-16 02:19:32 +0300

    2. Репозиторий под проект Tracker [#245623]

---
+++ src/main/java/ru/job4j/tracker/checkstyle/broken.java
@@ -0,0 +1,26 @@
+package ru.job4j.tracker.checkstyle;
+
+public class Broken {
+    private int sizeOfEmpty = 10;
+    public String surname;
+
+    public static final String NEW_VALUE = "";
+
+    void echo() {
+    }
+
+    void Media(Object obj) {
+        if (obj != null) {
+            System.out.println(obj);
+        }
+    }
+
+    void method(int a) {
+
+    }
+
+    String name;
+
+    Broken() {
+    }
+}
