74645e40d197038d6ea5bea037912334742129c9
Что такое import [#505278] <larinand97@gmail.com>
2025-05-07 00:28:07 +0300

    1.5. Поля объекта [#166083 #542402]

--- src/main/java/ru/job4j/oop/Cat.java
+++ src/main/java/ru/job4j/oop/Cat.java
@@ -3,19 +3,18 @@ package ru.job4j.oop;
 public class Cat {
 
     private String food;
-
-    public void show() {
-        System.out.println(this.name + " - " + this.food);
-    }
+    private String name;
 
     public void eat(String meat) {
         this.food = meat;
     }
 
-    private String name;
-
     public void giveNick(String nick) {
         this.name = nick;
+    }
+
+    public void show() {
+        System.out.println(this.name + " - " + this.food);
     }
 
     public static void main(String[] args) {
