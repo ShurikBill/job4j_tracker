8ebeaa5882ddce585eb4e5a1518f80d36126da25
Что такое import [#505278] <larinand97@gmail.com>
2025-05-07 00:33:51 +0300

    1.4. Вызов метода с возвращаемым типом. [#167387 #542400]

--- src/main/java/ru/job4j/oop/DummyDic.java
+++ src/main/java/ru/job4j/oop/DummyDic.java
@@ -3,13 +3,12 @@ package ru.job4j.oop;
 public class DummyDic {
 
     public String engToRus(String eng) {
-        String voice = eng;
-        return voice;
+        return "Неизвестное слово. " + eng;
     }
 
     public static void main(String[] args) {
         DummyDic tell = new DummyDic();
         String say = tell.engToRus("eng");
-        System.out.println("Неизвестное слово. " + say);
+        System.out.println(say);
     }
 }
