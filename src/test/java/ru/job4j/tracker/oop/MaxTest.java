b2ecd660268f7eda80cb3543c656707fef02972f
Что такое import [#505278] <larinand97@gmail.com>
2025-05-22 17:50:38 +0300

    3. Перегрузить метод max для трех чисел [#122644 #543778]

--- src/test/java/ru/job4j/tracker/oop/MaxTest.java
+++ src/test/java/ru/job4j/tracker/oop/MaxTest.java
@@ -24,21 +24,21 @@ public class MaxTest {
     @Test
     public void whenFirst10Second8Third4Max10() {
         Max max = new Max();
-        double result = max.maxMeaning(10.0,8.0,4.0);
+        double result = max.maxMeaning(10.0, 8.0, 4.0);
         assertThat(result).isEqualTo(10.0);
     }
 
     @Test
     public void whenFirst10Second8Third4Fourth12Max12() {
         Max max = new Max();
-        double result = max.maxMeaning(10.0,8.0,4.0, 12.0);
+        double result = max.maxMeaning(10.0, 8.0, 4.0, 12.0);
         assertThat(result).isEqualTo(12.0);
     }
 
     @Test
     public void whenFirst11Second15Third12Fourth13Max15() {
         Max max = new Max();
-        double result = max.maxMeaning(11.0,15.0,12.0, 13.0);
+        double result = max.maxMeaning(11.0, 15.0, 12.0, 13.0);
         assertThat(result).isEqualTo(15.0);
     }
 }
