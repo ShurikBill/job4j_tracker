d40e1ab634ab287ed4a09ed0c952b1c1bc8f258d
Что такое import [#505278] <larinand97@gmail.com>
2025-06-17 12:55:12 +0300

    7. Метод удаления заявки Tracker.delete [#211749]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -56,14 +56,10 @@ public class Tracker {
 
     public void delete(int id) {
         int index = indexOf(id);
-        if (index == -1) {
-            return;
+        if (index != -1) {
+            System.arraycopy(items,index + 1, items, index, size - index - 1);
+            items[size - 1] = null;
+            size--;
         }
-        int start = index + 1;
-        int distPos = index;
-        int length = size - index - 1;
-        items[size - 1] = null;
-        size--;
-        System.arraycopy(items, start, items, distPos, length);
     }
 }
