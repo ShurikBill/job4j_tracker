85bd01a660faa3eb02041363d6d8e5b344878f9b
Что такое import [#505278] <larinand97@gmail.com>
2025-06-26 17:56:47 +0300

    3.3. Реализация класса StartUI. Изменение заявки [#500746]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -39,8 +39,7 @@ public class StartUI {
                 } else {
                     System.out.println("Ошибка замены заявки.");
                 }
-            }
-            else if (select == 6) {
+            } else if (select == 6) {
                 run = false;
             }
         }
