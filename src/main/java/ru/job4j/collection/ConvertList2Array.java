6f154f6e71732a9a6d328283157b6fd1d4727698
Что такое import [#505278] <larinand97@gmail.com>
2025-09-01 17:18:36 +0300

    6. Конвертация ArrayList в двухмерный массив [#10035]

--- src/main/java/ru/job4j/collection/ConvertList2Array.java
+++ src/main/java/ru/job4j/collection/ConvertList2Array.java
@@ -12,7 +12,7 @@ public class ConvertList2Array {
         for (Integer number : list) {
             array[row][cell] = number;
             cell++;
-            if (cell >= cells){
+            if (cell >= cells) {
                 cell = 0;
                 row++;
             }
