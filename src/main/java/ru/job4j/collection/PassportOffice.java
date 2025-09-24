571e20fecab1fed602e745b11aa3ff30ea05577f
Что такое import [#505278] <larinand97@gmail.com>
2025-09-24 22:18:43 +0300

    1. Паспорт и жители [#212656]

---
+++ src/main/java/ru/job4j/collection/PassportOffice.java
@@ -0,0 +1,23 @@
+package ru.job4j.collection;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+public class PassportOffice {
+    private Map<String, Citizen> citizens = new HashMap<>();
+
+    public boolean add(Citizen citizen) {
+        String passport = citizen.getPassport();
+        if (!citizens.containsKey(passport)) {
+            citizens.put(citizen.getPassport(), citizen);
+            return true;
+        }
+        return false;
+    }
+
+
+    public Citizen get(String passport) {
+        return citizens.get(passport);
+    }
+}
