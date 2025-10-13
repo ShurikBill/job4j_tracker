215b5ca71485c8b0680bae89eb73ef1df92041dd
Что такое import [#505278] <larinand97@gmail.com>
2025-10-13 21:50:35 +0300

    0. Сортировка [#10096]

--- src/main/java/ru/job4j/tracker/ItemAscByName.java
+++ src/main/java/ru/job4j/tracker/ItemAscByName.java
@@ -4,7 +4,7 @@ import java.util.Comparator;
 
 public class ItemAscByName implements Comparator<Item> {
     @Override
-    public int compare (Item left, Item right) {
+    public int compare(Item left, Item right) {
         return left.getName().compareTo(right.getName());
     }
 }
--- src/main/java/ru/job4j/tracker/ItemDescByName.java
+++ src/main/java/ru/job4j/tracker/ItemDescByName.java
@@ -4,7 +4,7 @@ import java.util.Comparator;
 
 public class ItemDescByName implements Comparator<Item> {
     @Override
-    public int compare (Item left, Item right) {
+    public int compare(Item left, Item right) {
         return right.getName().compareTo(left.getName());
     }
 }
